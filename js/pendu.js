5251bf7e624deca205831f264e4e4e5ae55ce6d7
FaustinGarreau <fgarreau@edenschool.fr>
2019-10-02 17:31:21 +0200

    Nombre de chances marchent, pendu valide

--- js/pendu.js
+++ js/pendu.js
@@ -1,25 +1,46 @@
-var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+//var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var mots = ["code", "tableau", "editeur", "ligne", "format", "souris", "clavier", "touches", "ecran", "marque", "crayon", "nez", "cheveux", "video", "musique", "carnet", "stylo", "ecole", "codeur", "affiche"];
 var random = mots[Math.floor(Math.random() * mots.length)];
+var lettres = random.split('');
+var tab  =  [];
+
+for (i in lettres) {
+    tab.push("_");
+}
 console.log(random);
 var ban = [];
+var miss = 5;
+var chances = document.getElementById("fait");
 function goodword() {
+    if (miss == 0) {
+        alert("lost");
+        window.location.reload();
+        return;
+    }
     var input = document.getElementsByClassName("prompt")[0].value;
     var affiche = document.getElementById("lettres");
-    var inm = input.split('');
-    var xd = random.split('');
+    var stock = input.split('');
+    
+    var already = false;
     var non = [];
-        for (y in inm) {
-        for (let i = 0; i < xd.length; i++) {
-            if (inm[y] == xd[i] && !ban.includes(xd[i]) ) {  
-                non.push(xd[i]);
-                affiche.innerText +=xd[i], i ;
+        for (y in stock) {
+        for (let i = 0; i < lettres.length; i++) {
+            if (stock[y] == lettres[i] && !ban.includes(lettres[i]) ) {  
+                non.push(lettres[i]);
+                already = true;
+
+
+                tab[i] = lettres[i];
+                affiche.innerText = tab.join(" ");
+
             }
         }
+        
+    }
+    if (!already){
+        miss--;
+        fait.innerText = "Tu as " + miss + " " + "chances"; 
     }
     ban = ban.concat(non);
     console.log(ban);
-    
 }
-
-
